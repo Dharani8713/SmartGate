68f8d55e06ddfb900a33b4328a42fbb927d60f21
Kala-16 <kaladeepikavv069@gmail.com>
2025-11-04 18:51:21 +0530

    Updated

--- code1.py
+++ code1.py
@@ -1,17 +1,22 @@
-import serial
-import time
+int pirPin = 2;       // PIR sensor output pin
+int ledPin = 13;      // LED output pin
+int pirState = LOW;   // Start with no motion detected
+int val = 0;          // Variable for reading the pin status
 
-# Replace 'COM3' with your Arduino's port (e.g., '/dev/ttyACM0' on Linux)
-arduino_port = 'COM3'
-baud = 115200
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(pirPin, INPUT);
+  Serial.begin(9600);
+}
 
-ser = serial.Serial(arduino_port, baud)
-time.sleep(2)  # wait for Arduino to reset
+void loop() {
+  val = digitalRead(pirPin);  // Read input value
+  if (val == HIGH) {          // Check if motion detected
+    digitalWrite(ledPin, HIGH); // Turn LED on
+    if (pirState == LOW) {
+      Serial.println("Motion detected!");
+      pirState = HIGH;
+    }
+  } else {
+    digitalWrite(ledPin, LOW); // Turn LED o
 
-try:
-    while True:
-        if ser.in_waiting > 0:
-            data = ser.readline().decode().strip()
-            print(f"IR Sensor Output: {data}")
-except KeyboardInterrupt:
-    ser.close()
