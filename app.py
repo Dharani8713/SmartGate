f2ebd3fe199a279a7687f8fe3cc73c03cdc79639
Dharani M <dharani.mp13@gmail.com>
2025-11-22 21:08:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,8 @@ from firebase_admin import credentials, firestore, storage
 import json
 
 # ---------------- Firebase Initialization ----------------
-firebase_creds_json = st.secrets["FIREBASE_CREDENTIALS"]["json"]
-cred_dict = json.loads(firebase_creds_json)  # Now valid JSON
-cred = credentials.Certificate(cred_dict)    
+cred = credentials.Certificate("serviceAccountKey.json")
+   
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'smart-gate-52e2d.firebasestorage.app'
 })
@@ -58,6 +57,7 @@ if uploaded_file:
         if user_contact:
             # Example: replace with Twilio or SMTP integration
             st.write(f"Notification sent to {user_contact} (placeholder)")
+
 
 
 
